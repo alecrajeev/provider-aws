de54d891e446471611b0b8c8622af08717fd82df
Alec Rajeev <alecinthecloud@gmail.com>
2022-01-02 20:29:24 -0500

    Added example

--- pkg/controller/elasticsearchservice/elasticsearchdomain/setup.go
+++ pkg/controller/elasticsearchservice/elasticsearchdomain/setup.go
@@ -1,10 +1,14 @@
 package elasticsearchdomain
 
 import (
+	"context"
 	"time"
 
+	"github.com/aws/aws-sdk-go/aws"
+	svcsdk "github.com/aws/aws-sdk-go/service/elasticsearchservice"
 	"github.com/crossplane/crossplane-runtime/pkg/event"
 	"github.com/crossplane/crossplane-runtime/pkg/logging"
+	"github.com/crossplane/crossplane-runtime/pkg/meta"
 	"github.com/crossplane/crossplane-runtime/pkg/ratelimiter"
 	"k8s.io/client-go/util/workqueue"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
@@ -19,6 +23,11 @@ import (
 // SetupElasticsearchDomain adds a controller that reconciles ElasticsearchDomain.
 func SetupElasticsearchDomain(mgr ctrl.Manager, l logging.Logger, rl workqueue.RateLimiter, poll time.Duration) error {
 	name := managed.ControllerName(svcapitypes.ElasticsearchDomainKind)
+	opts := []option{
+		func(e *external) {
+			e.preObserve = preObserve
+		},
+	}
 	return ctrl.NewControllerManagedBy(mgr).
 		Named(name).
 		WithOptions(controller.Options{
@@ -27,8 +36,14 @@ func SetupElasticsearchDomain(mgr ctrl.Manager, l logging.Logger, rl workqueue.R
 		For(&svcapitypes.ElasticsearchDomain{}).
 		Complete(managed.NewReconciler(mgr,
 			resource.ManagedKind(svcapitypes.ElasticsearchDomainGroupVersionKind),
-			managed.WithExternalConnecter(&connector{kube: mgr.GetClient()}),
+			managed.WithExternalConnecter(&connector{kube: mgr.GetClient(), opts: opts}),
+			managed.WithInitializers(),
 			managed.WithPollInterval(poll),
 			managed.WithLogger(l.WithValues("controller", name)),
 			managed.WithRecorder(event.NewAPIRecorder(mgr.GetEventRecorderFor(name)))))
+}
+
+func preObserve(_ context.Context, cr *svcapitypes.ElasticsearchDomain, obj *svcsdk.DescribeElasticsearchDomainInput) error {
+	obj.DomainName = aws.String(meta.GetExternalName(cr))
+	return nil
 }
